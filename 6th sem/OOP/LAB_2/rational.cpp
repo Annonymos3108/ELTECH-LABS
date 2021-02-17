398091723a6408dcef16793afc97499510860d74
Annonymos3108 <anersisyan1999@mail.ru>
2021-02-17 21:48:17 +0300

    Added LAB_2 project files

---
+++ 6th sem/OOP/LAB_2/rational.cpp
@@ -0,0 +1,6 @@
+#include "rational.h"
+
+Rational::Rational(int a, unsigned int b) {
+    this->a = a;
+    this->b = b;
+}
---
+++ 6th sem/OOP/LAB_2/rational.h
@@ -0,0 +1,13 @@
+#ifndef RATIONAL_H
+#define RATIONAL_H
+
+class Rational {
+private:
+    int a;
+    unsigned int b;
+public:
+    Rational(int, unsigned int);
+
+};
+
+#endif
