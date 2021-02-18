fda896f0f5fadfff9436ccac0dd84dd17aa0c953
Arthur Nersisyan <arthur-nersisyan@hotmail.com>
2021-02-18 12:34:52 +0300

    Fixed OS LAB 1 code

--- 6th sem/OS/LAB_1/lab1.cpp
+++ 6th sem/OS/LAB_1/lab1.cpp
@@ -14,48 +14,51 @@ struct pthread_args {
 void *thread_func_1(void *arg)
 {
     printf("Thread 1 was started...\n");
-    pthread_args* args = (pthread_args*)arg;
-    while (args->flag)
+    bool* flag = (bool*)arg;
+    while (!*flag)
     {
         printf("1\n");
         sleep(1);
     }
+    int* returnCode = new int(2);
     printf("Thread 1 was ended...\n");
-    pthread_exit((void*)1);
+    pthread_exit(returnCode);
 }
 
 //func for thread 2
 void *thread_func_2(void *arg)
 {
     printf("Thread 2 was started...\n");
-    pthread_args* args = (pthread_args*)arg;
-    while (args->flag)
+    bool* flag = (bool*)arg;
+    while (!*flag)
     {
         printf("2\n");
         sleep(1);
     }
+    int* returnCode = new int(3);
     printf("Thread 2 was ended...\n");
-    pthread_exit((void*)2);
+    pthread_exit(returnCode);
 }
 
 
 int main(int argc, char *argv[])
 {
-    int thread_1_exitcode = 0, thread_2_exitcode = 0, result;
+    bool flag1 = false;
+    bool flag2 = false;
+
     pthread_t thread1, thread2;
-    pthread_args arg1, arg2;
+   
+    int* returnCode1, *returnCode2;
 
-    arg1.flag = true;
-    arg2.flag = true;
+    int result;
   
-
     //create
-    result = pthread_create(&thread1, NULL, thread_func_1, &arg1);
+    result = pthread_create(&thread1, NULL, thread_func_1, (void*)&flag1);
     if (result != 0) {
         perror("Error on creating the 1st thread...\n");
         return -1;
     }
-    result = pthread_create(&thread2, NULL, thread_func_2, &arg2);
+    result = pthread_create(&thread2, NULL, thread_func_2, (void*)&flag2);
     if (result != 0) {
         perror("Error on creating the 2nd thread...\n");
         return -1;
@@ -66,24 +69,28 @@ int main(int argc, char *argv[])
     getchar();
 
     //install flags
-    arg1.flag = false;
-    arg2.flag = false;
+    flag1 = true;
+    flag2 = true;
 
     //join
-    result = pthread_join(thread1, (void **)&thread_1_exitcode);
+    result = pthread_join(thread1, (void **)&returnCode1);
     if (result != 0) {
         perror("Error on joining the 1st thread...\n");
         return -1;
     }
-    result = pthread_join(thread2, (void **)&thread_2_exitcode);
+    result = pthread_join(thread2, (void **)&returnCode2);
     if (result != 0) {
         perror("Error on joining the 2nd thread...\n");
         return -1;
     }
 
     //print exit codes and exit
-    printf("Thread 1 exited with code: %i \n", thread_1_exitcode);
-    printf("Thread 2 exited with code: %i \n", thread_2_exitcode);
+    printf("Thread 1 exited with code: %d \n", *returnCode1);
+    printf("Thread 2 exited with code: %d \n", *returnCode2);
+    
+    delete returnCode1;
+    delete returnCode2;
+
     printf("Done!\n");
     return 0;
 }
