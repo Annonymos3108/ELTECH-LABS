c796aaed6125ffe8aee17a7bda3111dcd882fe66
Annonymos3108 <anersisyan1999@mail.ru>
2021-02-24 18:24:55 +0300

    Added 2nd lab

--- 6th-Sem/OS/LAB_2/lab2_2.cpp
+++ 6th-Sem/OS/LAB_2/lab2_2.cpp
@@ -1,106 +1,145 @@
-//lab 2_2
+//lab 2_2_trywait // semaphore
+//8363 NersisyanA
 
+#include <stdlib.h>
 #include <stdio.h>
-#include <pthread.h>
 #include <unistd.h>
-#include <string.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <time.h>
 #include <errno.h>
 
-bool            flag1 = false;
-bool            flag2 = false;
-pthread_mutex_t mutex;
+sem_t sem_id;
 
-
-void* function1(void* arg) {
-    printf("thread1\n");
-
+//func for thread 1
+void *thread_func_1(void *arg)
+{
+    printf("\nThread 1 was started...\n");
     bool* flag = (bool*)arg;
+    while (!*flag)
+    {
+        //trywait
+        int ret =  sem_trywait(&sem_id);
+        if(ret == 0) {
+            //critical section 
+            for (int i = 0; i < 5; i++) {
+                printf("1");
+                fflush(stdout);
+                sleep(1);
+            }
+            //end of critical section
 
-    while (!*flag) {
-        int blockValue = pthread_mutex_trylock(&mutex);
-
-        if (blockValue != 0) {
-            printf("Error from thread1: %s\n", strerror(blockValue));
-            sleep(1);
-            continue;
+            ret = sem_post(&sem_id);
+            if (ret == 0) {
+                sleep(1);
+            }
+            else {
+                perror("sem_post");
+                //exit(EXIT_FAILURE);
+            }
         }
-
-        for (int i = 0; i < 3; i++) {
-            printf("1 in progress\n");
-            fflush(stdout);
-            sleep(1);
+        // error on trywait
+        else {
+            perror("\nsem_trywait");
+            //exit(EXIT_FAILURE);
         }
-
-        pthread_mutex_unlock(&mutex);
-        sleep(1);
     }
-
-    int* returnCode = new int(2);
-    printf("end1\n");
-    pthread_exit(returnCode);
-}
-
-void* function2(void* arg) {
-    printf("thread2\n");
-
-    bool* flag = (bool*)arg;
-
-    while (!*flag) {
-        int blockValue = pthread_mutex_trylock(&mutex);
-
-        if (blockValue != 0) {
-            printf("Error from thread2: %s\n", strerror(blockValue));
-            sleep(1);
-            continue;
-        }
-
-        for (int i = 0; i < 3; i++) {
-            printf("2 in progress\n");
-            fflush(stdout);
-            sleep(1);
-        }
-
-        pthread_mutex_unlock(&mutex);
-        sleep(1);
-    }
-
     int* returnCode = new int(3);
-    printf("end2\n");
+    printf("\nThread 1 was ended...\n");
+    pthread_exit(returnCode);
+}
+
+//func for thread 2
+void *thread_func_2(void *arg)
+{
+    printf("\nThread 2 was started...\n");
+    bool* flag = (bool*)arg;
+    while (!*flag)
+    {
+        int ret =  sem_trywait(&sem_id);
+        if(ret == 0) {
+            for (int i = 0; i < 5; i++) {
+                printf("2");
+                fflush(stdout);
+                sleep(1);
+            }
+
+            ret = sem_post(&sem_id);
+            if (ret == 0) {
+                sleep(1);
+            }
+            else {
+                perror("sem_post");
+                //exit(EXIT_FAILURE);
+            }
+        }
+        else {
+            perror("\nsem_trywait");
+            //exit(EXIT_FAILURE);
+        }
+    }
+    int* returnCode = new int(4);
+    printf("\nThread 2 was ended...\n");
     pthread_exit(returnCode);
 }
 
 
-int main() {
-    printf("thread0\n");
+int main()
+{
+    int result;
+    int* returnCode1, *returnCode2;
+    bool flag1 = false, flag2 = false;
+    pthread_t thread1, thread2;
+    
+    //create semaphore
+    result = sem_init(&sem_id, 0, 1);
+    if (result != 0) {
+        printf("\nError on creating the 1st semaphore. Error code %i", result);
+        return -1;
+    }
 
-    pthread_mutex_init(&mutex, NULL);
+    //create threads
+    result = pthread_create(&thread1, NULL, thread_func_1, (void*)&flag1);
+    if (result != 0) {
+        printf("\nError on creating the 1st thread. Error code %i", result);
+        return -1;
+    }
+    result = pthread_create(&thread2, NULL, thread_func_2, (void*)&flag2);
+    if (result != 0) {
+        printf("\nError on creating the 2nd thread. Error code %i", result);
+        return -1;
+    }
 
-    pthread_t t1;
-    pthread_t t2;
-
-    pthread_create(&t1, NULL, function1, (void*)&flag1);
-    pthread_create(&t2, NULL, function2, (void*)&flag2);
-
+    //wainting... press any key
     getchar();
+    printf("\nJoining threads process starts...\n");
 
+    //install flags
     flag1 = true;
     flag2 = true;
 
-    int* returnCode1;
-    int* returnCode2;
+    //join
+    result = pthread_join(thread1, (void**)&returnCode1);
+    if (result != 0) {
+        printf("\nError on joining the 1st thread. Error code %i\n", result);
+        return -1;
+    }
+    result = pthread_join(thread2, (void**)&returnCode2);
+    if (result != 0) {
+        printf("\nError on joining the 2nd thread. Error code %i\n", result);
+        return -1;
+    }
 
-    pthread_join(t1, (void**)&returnCode1);
-    pthread_join(t2, (void**)&returnCode2);
-
-    printf("thread1 finished with exit code %d\n", *returnCode1);
-    printf("thread2 finished with exit code %d\n", *returnCode2);
-
-    pthread_mutex_destroy(&mutex);
+    //print exit codes and exit
+    printf("Thread 1 exited with code: %d \n", *returnCode1);
+    printf("Thread 2 exited with code: %d \n", *returnCode2);
+    
+    sem_destroy(&sem_id);
+    printf("Semaphore destroyed!\n");
 
     delete returnCode1;
     delete returnCode2;
 
-    printf("end0\n");
-
+    printf("Done!\n");
     return 0;
 }
