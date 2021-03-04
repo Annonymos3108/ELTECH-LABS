920150af4b536a9e58928cd96007c4446ef4c820
Arthur Nersisyan <24681914+Annonymos3108@users.noreply.github.com>
2021-03-04 03:54:00 +0300

    Fixed exec5_1.sh

--- 6th-Sem/OS/LAB_5/exec5_1.sh
+++ 6th-Sem/OS/LAB_5/exec5_1.sh
@@ -4,19 +4,7 @@ cat /dev/null > output.txt
 g++ lab5_1.cpp -lpthread -o lab5_1
 g++ lab5_2.cpp -lpthread -o lab5_2
 
-#if you work in Xfce
-#xterm -- ./lab5_1
-#xterm -- ./lab5_2
-
-#xterm -- tail --pid=$(pidof lab5_1 || pidof lab5_2) -f output.txt
-
-#if you work in KDE
-konsole -- ./lab5_1
-konsole -- ./lab5_2
-
-konsole -- tail --pid=$(pidof lab5_1 || pidof lab5_2) -f output.txt
-
 #if you work in Gnome
-#gnome-terminal -- ./lab5_1
-#gnome-terminal -- ./lab5_2
-#gnome-terminal -- tail --pid=$(pidof lab5_1 || pidof lab5_2) -f output.txt
+gnome-terminal -- ./lab5_1
+gnome-terminal -- ./lab5_2
+gnome-terminal -- tail --pid=$(pidof lab5_1 || pidof lab5_2) -f output.txt
